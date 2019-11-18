7211b3cb4ce91cab83321710a75ff2e59dd58b29
naskoap <naskoap@gmail.com>
2019-11-18 00:20:36 -0500

    Added geocoding and a reverse-route feature

--- js/app.js
+++ js/app.js
@@ -1,45 +1,73 @@
-let center = L.latLng(40.735864, -73.980492);
-var map = L.map('map').setView(center, 12);
+new Vue({
+  el: '#map',
+  data: {
+    map: null,
+    tileLayer: null,
+    control: null,
+    plan: null,
+    startButton: null,
+    destButton: null,
+    layers: []
+  },
+  mounted() {
+    this.initMap();
+    this.initLayers();
+    this.reverseRoute();
+    this.map.on('click', this.onMapClick);
+  },
+  methods: {
+    initMap() {
+      let latlng = L.latLng(40.735864, -73.980492);
+      this.map = L.map('map').setView(latlng, 11);
+      this.tileLayer = L.tileLayer(
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+        {
+          maxZoom: 18,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+        }
+      ).addTo(this.map);
+    },
+    reverseRoute() {
+      this.plan = new ReversablePlan([
+            L.latLng(40.702518, -74.014961), //Battery Park
+            L.latLng(40.730623, -73.997602) //Washington Sq. Park
+        ], {
+            geocoder: L.Control.Geocoder.nominatim(), //simple geocoder form to locate places
+            routeWhileDragging: true
+        });
 
-L.tileLayer(
-  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-  {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-  }
-).addTo(map);
+        this.control = L.Routing.control({
+            routeWhileDragging: true,
+            collapsible: true,
+            plan: this.plan
+        }).addTo(this.map);
+    },
+    onMapClick(e) {
+      let container = L.DomUtil.create('div'),
+          c = this.control, //instantiate local control & map
+          m = this.map;
 
-var control = L.Routing.control({
-  waypoints: [
-    L.latLng(40.702518, -74.014961), //Battery Park
-    L.latLng(40.730623, -73.997602) //Washington Sq. Park
-  ]
-}).addTo(map);
+      this.startButton = createButton('Start from this location', container);
+      this.destButton = createButton('Go to this location', container);
 
-function createButton(label, container) {
-  var btn = L.DomUtil.create('button', '', container);
-  btn.setAttribute('type', 'button');
-  btn.innerHTML = label;
-  return btn;
-};
+      L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(this.map);
 
-map.on('click', function(e) {
-  let container = L.DomUtil.create('div'),
-      startBtn = createButton('Start from this location', container),
-      destBtn = createButton('Go to this location', container);
+      //remove the waypoint at index 0 (the first), and then add another one at the clicked location
+      L.DomEvent.on(this.startButton, 'click', function() {
+        c.spliceWaypoints(0, 1, e.latlng);
+        m.closePopup();
+      });
 
-  L.popup()
-    .setContent(container)
-    .setLatLng(e.latlng)
-    .openOn(map);
+      //replace the last waypoint with a new one at the clicked location
+      L.DomEvent.on(this.destButton, 'click', function() {
+        c.spliceWaypoints(c.getWaypoints().length - 1, 1, e.latlng);
+        m.closePopup();
+      });
+    },
+    initLayers() {}
+  },
 
-  L.DomEvent.on(startBtn, 'click', function() {
-    control.spliceWaypoints(0, 1, e.latlng);
-    map.closePopup();
-  });
-
-  L.DomEvent.on(destBtn, 'click', function() {
-    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-    map.closePopup();
-  });
 });
--- js/utils.js
+++ js/utils.js
@@ -4,3 +4,16 @@ function createButton(label, container) {
   btn.innerHTML = label;
   return btn;
 };
+
+//reverse the direction of the route
+var ReversablePlan = L.Routing.Plan.extend({
+    createGeocoders: function() {
+        var container = L.Routing.Plan.prototype.createGeocoders.call(this),
+            reverseButton = createButton('Reverse', container);
+        L.DomEvent.on(reverseButton, 'click', function() {
+          var waypoints = this.getWaypoints();
+          this.setWaypoints(waypoints.reverse());
+        }, this);
+        return container;
+    }
+});
